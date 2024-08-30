6bc11c0f37ae0708ac2bb8ac1cc8338689795ba3
Krystian <krystian.sobczyk5642@wp.pl>
2024-08-30 14:00:22 +0200

    removed files

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AlgorithmVisualizer_FILE C:/Users/krystian.sobczyk/Music/AlgorithmVisualizer/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/AlgorithmVisualizer.exe)
-set(__QT_DEPLOY_TARGET_AlgorithmVisualizer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AlgorithmVisualizer_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
